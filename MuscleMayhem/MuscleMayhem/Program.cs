e3b0bd0cbfce53693b709604386fe17c78cf36ce
Adamka0111 <62609739+Adamka0111@users.noreply.github.com>
2024-03-23 17:45:16 +0100

    Tutorial szoba alapja
    egyébb: főmenü design változtatása

--- MuscleMayhem/MuscleMayhem/Program.cs
+++ MuscleMayhem/MuscleMayhem/Program.cs
@@ -12,22 +12,23 @@ class Program
     static void MainMenu()
     {
         Console.Clear();
+        Console.WriteLine("__   __ __    __  ___   ____ __    _____\r\n||\\ /|| ||    || //    //    ||    ||\r\n||\\V/|| ||    || \\\\__  ||    ||    ||__\r\n|| V || ||    ||    \\\\ ||    ||    ||\r\n||   ||  \\\\__//   __// \\\\___ ||___ ||___\r\n__   __\t  ____  __   __ __   __ _____ __   __\r\n||\\ /||   //\\\\   \\\\ //  ||   || ||    ||\\ /||\r\n||\\V/||  //  \\\\   \\V/   ||===|| ||__  ||\\V/||\r\n|| V || ||====||   V    ||   || ||    || V ||\r\n||   || ||    ||   |    ||   || ||___ ||   ||\r\n");
         Console.WriteLine("=== Menü ===");
         Console.WriteLine("1. Játék");
         Console.WriteLine("2. Tutorial");
-        Console.WriteLine("3. Controls");
+        Console.WriteLine("3. Irányítás");
         Console.WriteLine("4. Kilépés");
         Console.Write("Válassz egy lehetőséget: ");
 
         string choice = Console.ReadLine();
-
+        
         switch (choice)
         {
             case "1":
                 StartGame();
                 break;
             case "2":
-                ShowTutorial();
+                StartTutorial();
                 break;
             case "3":
                 ShowControls();
@@ -43,25 +44,160 @@ class Program
         }
     }
 
-    static void ShowTutorial()
+    static void ShowControls()
     {
         Console.Clear();
-        Console.WriteLine("=== Tutorial ===");
-        Console.WriteLine("Ez lenne a játék tutorial-ja.");
+        Console.WriteLine("=== Irányítás ===");
+        Console.WriteLine("Mozgás: WASD");
+        Console.WriteLine("Interakció: Enter");
         Console.WriteLine("Nyomj egy billentyűt a visszalépéshez a menübe...");
         Console.ReadKey();
         MainMenu();
     }
 
-    static void ShowControls()
+    static void StartTutorial()
     {
-        Console.Clear();
-        Console.WriteLine("=== Controls ===");
-        Console.WriteLine("Mozgás: WASD vagy Nyilakí");
-        Console.WriteLine("Nyomj egy billentyűt a visszalépéshez a menübe...");
-        Console.ReadKey();
-        MainMenu();
+        Console.WriteLine("tutorial szoba");
+
+        int startX = 4;
+        int startY = 9;
+        int currentX = startX;
+        int currentY = startY;
+
+        int width = 10;
+        int height = 10;
+        char[,] map = new char[height, width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (i == 0 && (j == 4 || j == 5))
+                {
+                    map[i, j] = '■';
+                }
+                else if (j == 0 && (i == 4 || i == 5))
+                {
+                    map[i, j] = '■';
+                }
+                else if (j == 9 && ( i == 4 || i == 5))
+                {
+                    map[i, j] = '■';
+                }
+                else
+                {
+                    map[i, j] = '·';
+                }
+            }
+        }
+
+        map[currentY, currentX] = 'O';
+
+        DrawMap(map, width, height);
+
+        Console.WriteLine("Ez a tutorial szoba.\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
+
+        int szoba = 0;
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.W:
+                    if (currentY > 0)
+                    {
+                        if ((currentY == 0 && currentX == 4) || (currentY == 0 && currentX == 5) || (currentY == 4 && currentX == 0) || (currentY == 5 && currentX == 0) || (currentY == 4 && currentX == 9) || (currentY == 5 && currentX == 9))
+                        {
+                            map[currentY, currentX] = '■';
+                        }
+                        else
+                        {
+                            map[currentY, currentX] = '·';
+                        }
+                        currentY--;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (currentY < height - 1)
+                    {
+                        if ((currentY == 0 && currentX == 4) || (currentY == 0 && currentX == 5) || (currentY == 4 && currentX == 0) || (currentY == 5 && currentX == 0) || (currentY == 4 && currentX == 9) || (currentY == 5 && currentX == 9))
+                        {
+                            map[currentY, currentX] = '■';
+                        }
+                        else
+                        {
+                            map[currentY, currentX] = '·';
+                        }
+                        currentY++;
+                    }
+                    break;
+                case ConsoleKey.A:
+                    if (currentX > 0)
+                    {
+                        if ((currentY == 0 && currentX == 4) || (currentY == 0 && currentX == 5) || (currentY == 4 && currentX == 0) || (currentY == 5 && currentX == 0) || (currentY == 4 && currentX == 9) || (currentY == 5 && currentX == 9))
+                        {
+                            map[currentY, currentX] = '■';
+                        }
+                        else
+                        {
+                            map[currentY, currentX] = '·';
+                        }
+                        currentX--;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    if (currentX < width - 1)
+                    {
+                        if ((currentY == 0 && currentX == 4) || (currentY == 0 && currentX == 5) || (currentY == 4 && currentX == 0) || (currentY == 5 && currentX == 0) || (currentY == 4 && currentX == 9) || (currentY == 5 && currentX == 9))
+                        {
+                            map[currentY, currentX] = '■';
+                        }
+                        else
+                        {
+                            map[currentY, currentX] = '·';
+                        }
+                        currentX++;
+                    }
+                    break;
+                case ConsoleKey.Enter:
+                    if (currentY == 0 && (currentX == 4 || currentX == 5))
+                    {
+                        szoba = 1;
+                    }
+                    else if (currentX == 0 && (currentY == 4 || currentY == 5))
+                    {
+                        szoba = 2;
+                    }
+                    else if (currentX == 9 && (currentY == 4 || currentY == 5)){
+                        szoba = 3;
+                    }
+                    break;
+                case ConsoleKey.Escape:
+                    MainMenu();
+                    break;
+            }
+
+            map[currentY, currentX] = 'O';
+
+            DrawMap(map, width, height);
+
+            Console.WriteLine("Ez a tutorial szoba\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
+
+            switch (szoba)
+            {
+                case 1:
+                    CombatBoss();
+                    break;
+                case 2:
+                    ShopMenu();
+                    break;
+                case 3:
+                    CombatEnemy();
+                    break;
+            }
+        }
     }
+
     static void StartGame()
     {
         int startX = 0;
@@ -76,7 +212,7 @@ class Program
         {
             for (int j = 0; j < width; j++)
             {
-                map[i, j] = '.';
+                map[i, j] = '·';
             }
         }
 
@@ -84,7 +220,6 @@ class Program
 
         DrawMap(map, width, height);
 
-        // Mozgások
         while (true)
         {
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
@@ -93,57 +228,71 @@ class Program
                 case ConsoleKey.UpArrow:
                     if (currentY > 0)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentY--;
                     }
                     break;
                 case ConsoleKey.DownArrow:
                     if (currentY < height - 1)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentY++;
                     }
                     break;
                 case ConsoleKey.LeftArrow:
                     if (currentX > 0)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentX--;
                     }
                     break;
                 case ConsoleKey.RightArrow:
                     if (currentX < width - 1)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentX++;
                     }
                     break;
                 case ConsoleKey.W:
                     if (currentY > 0)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentY--;
                     }
                     break;
                 case ConsoleKey.S:
                     if (currentY < height - 1)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentY++;
                     }
                     break;
                 case ConsoleKey.A:
                     if (currentX > 0)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentX--;
                     }
                     break;
                 case ConsoleKey.D:
                     if (currentX < width - 1)
                     {
-                        map[currentY, currentX] = '.';
+                        map[currentY, currentX] = '·';
                         currentX++;
+                    }
+                    break;
+                case ConsoleKey.Enter:
+                    if (currentX == 0 && (currentY == 4 || currentY == 5))
+                    {
+                        ShopMenu();
+                    }
+                    else if (currentX == 9 && (currentY == 4 || currentY == 5))
+                    {
+                        CombatEnemy();
+                    }
+                    else if (currentY == 0 && (currentX == 4 || currentX == 5))
+                    {
+                        CombatBoss();
                     }
                     break;
                 case ConsoleKey.Escape:
@@ -155,6 +304,25 @@ class Program
             DrawMap(map, width, height);
         }
     }
+
+    static void ShopMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("bolt");
+    }
+
+    static void CombatEnemy()
+    {
+        Console.Clear();
+        Console.WriteLine("enemy");
+    }
+
+    static void CombatBoss()
+    {
+        Console.Clear();
+        Console.WriteLine("boss");
+    }
+
     static void DrawMap(char[,] map, int width, int height)
     {
         Console.Clear();
@@ -162,7 +330,16 @@ class Program
         {
             for (int j = 0; j < width; j++)
             {
-                Console.Write($" {map[i, j] }");
+                if (map[i, j] == '■')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($" {map[i, j]}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write($" {map[i, j]}");
+                }
             }
             Console.WriteLine();
         }
