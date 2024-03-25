25811d8377846c4c5e74c9028c7b971a60fd59f0
farkasgergely <100405796+farkasgergely@users.noreply.github.com>
2024-03-25 18:56:56 +0100

    shop

--- MuscleMayhem/MuscleMayhem/Program.cs
+++ MuscleMayhem/MuscleMayhem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Net;
 
-class Program
+class Game
 {
     static void Main()
     {
@@ -8,6 +9,16 @@ class Program
 
         MainMenu();
     }
+
+    static bool Tutorial;
+    static int Room;
+    static int Item1;
+    static int Item2;
+    static int Item3;
+    static int Item4;
+    static int Item5;
+    static int Item6;
+    static bool hasItem;
 
     static void MainMenu()
     {
@@ -18,6 +29,16 @@ class Program
 
         while (true)
         {
+            bool Tutorial = false;
+            int Room = 0;
+            int Item1 = 0;
+            int Item2 = 0;
+            int Item3 = 0;
+            int Item4 = 0;
+            int Item5 = 0;
+            int Item6 = 0;
+            bool hasItem = false;
+
             Console.Clear();
             Console.WriteLine("| __   __ __    __  ___   ____ __    _____\t\r\n| ||\\ /|| ||    || //    //    ||    ||\t\t\r\n| ||\\V/|| ||    || \\\\__  ||    ||    ||==\t    ,--.--._\r\n| || V || ||    ||    \\\\ ||    ||    ||\t\t  -\" _, \\___)\r\n| ||   ||  \\\\__//   __// \\\\___ ||___ ||___\t     / _/____)\r\n| __   __   ____  __    __ __   __ _____ __   __     \\//(____)\r\n| ||\\ /||   //\\\\   \\\\  //  ||   || ||    ||\\ /||  -\\     (__)\r\n| ||\\V/||  //  \\\\   \\\\//   ||===|| ||==  ||\\V/||    `-----\"\r\n| || V || ||====||   ||    ||   || ||    || V || \r\n| ||   || ||    ||   ||    ||   || ||___ ||   || \r\n|\t\t\t\t\t\t");
             Console.WriteLine("============================|Menü|============================");         
@@ -26,8 +47,8 @@ class Program
 
             Console.WriteLine("==============================================================");
 
-            ConsoleKeyInfo menuControll = Console.ReadKey(true);
-            switch (menuControll.Key)
+            ConsoleKeyInfo menuSelect = Console.ReadKey(true);
+            switch (menuSelect.Key)
             {
                 case ConsoleKey.W:
                     menu[menuPoint, 0] = " ";
@@ -62,7 +83,7 @@ class Program
             switch (menuChoice)
             {
                 case 1:
-                    StartGame();
+                    Cutscene1();
                     break;
                 case 2:
                     StartTutorial();
@@ -76,6 +97,19 @@ class Program
             }
 
         }
+    }
+
+    static void Cutscene1()
+    {
+        Console.Clear();
+        Console.WriteLine("==============================================================");
+        Console.WriteLine(" \t _______\t  |         |          _______     |\r\n \t |  |  |\t  |G   Y   M|\t       |  |  |     |\r\n \t |--+--|          |_________|          |--+--|     |\r\n \t |__|__|\t\t\t       |__|__|     |\r\n \t\t\t    _______\t\t\t   |\r\n \t\t\t   |   |   |\t\t___|__     |\r\n      _____ ___|___\t   | · | · |\t\t_|___|___  |\r\n_____/     \\_|_____________|___|___|___________________|___|_ \r\n     |     |\r\n     \\_____/\r\n        |");
+        Console.WriteLine("--------------------------------------------------------------");
+        Console.WriteLine("Tegnap erősebben edzettél mint ebmer valaha edzett, viszont m-\negirigyelt az edzőterem tulaja, és visszavonta a tagságodat.\nIlyen bűn személyes bosszút követel. Megmutatod neki, hogy m-\nilyen erős vagy, és nem hagyod hogy bárki az utadba álljon.");
+        Console.WriteLine("Nyomj egy billentyűt a folytatáshoz...");
+        Console.WriteLine("==============================================================");
+        Console.ReadKey();
+        StartGame();
     }
 
     static void ShowControls()
@@ -93,6 +127,7 @@ class Program
 
     static void StartTutorial()
     {
+        Tutorial = true;
         int startX = 4;
         int startY = 9;
         int currentX = startX;
@@ -124,12 +159,9 @@ class Program
             }
         }
 
-        map[currentY, currentX] = 'O';
+        map[currentY, currentX] = 'O';       
 
-
-        Console.WriteLine("Ez a tutorial szoba.\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
-
-        int szoba = 0;
+        Room = 0;
 
         while (true)
         {
@@ -137,8 +169,10 @@ class Program
             Console.WriteLine("Szoba: tutorial");
             Console.WriteLine("---------------------");
             DrawMap(map, width, height);
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            switch (keyInfo.Key)
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Ez a tutorial szoba.\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
+            ConsoleKeyInfo tutorialControl = Console.ReadKey(true);
+            switch (tutorialControl.Key)
             {
                 case ConsoleKey.W:
                     if (currentY > 0)
@@ -199,14 +233,14 @@ class Program
                 case ConsoleKey.Enter:
                     if (currentY == 0 && (currentX == 4 || currentX == 5))
                     {
-                        szoba = 1;
+                        Room = 1;
                     }
                     else if (currentX == 0 && (currentY == 4 || currentY == 5))
                     {
-                        szoba = 2;
+                        Room = 2;
                     }
                     else if (currentX == 9 && (currentY == 4 || currentY == 5)){
-                        szoba = 3;
+                        Room = 3;
                     }
                     break;
                 case ConsoleKey.Escape:
@@ -218,9 +252,9 @@ class Program
 
             DrawMap(map, width, height);
 
-            Console.WriteLine("Ez a tutorial szoba\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
+            Console.WriteLine("Ez a tutorial szoba.\nJobb oldalt van a bolt, ahol itemeket tudsz venni.\nBal oldalt van az edzőtér, ahol harcolhatsz egy ellenféllel.\nFent van a folyosó a következő szobához, ahol egy bossal harcolatsz.");
 
-            switch (szoba)
+            switch (Room)
             {
                 case 1:
                     CombatBoss();
@@ -237,121 +271,130 @@ class Program
 
     static void StartGame()
     {
-        int startX = 0;
-        int startY = 0;
-        int currentX = startX;
-        int currentY = startY;
         
-        int width = 10;
-        int height = 10;
-        char[,] map = new char[height, width];
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                map[i, j] = '·';
-            }
-        }
+    }
 
-        map[startY, startX] = 'O';
-
-        DrawMap(map, width, height);
+    static void ShopMenu()
+    {
+        string[,] shop = new string[,] { { " ", "Súllyozott boxkesztyűk (ütés bónusz)" }, { " ", "Kar védők (blokk bónusz)" }, { " ", "Edző cipők (kitérés bónusz)" }, { " ", "Testépítő olaj (flex bónusz)" }, { " ", "Étrendkiegészítők (életerő bónusz)" }, { " ", "Energiaital (energia bónusz))" }, { " ", "Vissza" } };
+        int shopPoint = 0;
+        int shopChoice = 0;
+        shop[shopPoint, 0] = ">";
 
         while (true)
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            switch (keyInfo.Key)
+            Console.Clear();
+            Console.WriteLine("===================================================================");
+            if (hasItem)
             {
-                case ConsoleKey.UpArrow:
-                    if (currentY > 0)
-                    {
-                        map[currentY, currentX] = '·';
-                        currentY--;
-                    }
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (currentY < height - 1)
-                    {
-                        map[currentY, currentX] = '·';
-                        currentY++;
-                    }
-                    break;
-                case ConsoleKey.LeftArrow:
-                    if (currentX > 0)
-                    {
-                        map[currentY, currentX] = '·';
-                        currentX--;
-                    }
-                    break;
-                case ConsoleKey.RightArrow:
-                    if (currentX < width - 1)
-                    {
-                        map[currentY, currentX] = '·';
-                        currentX++;
-                    }
-                    break;
+                Console.WriteLine("Már van egy itemed, többet nem bír a szervezeted.");
+            }
+            else
+            {
+                Console.WriteLine("Mivel nincs pénzed, az ilyedt kasszás megengedi neked hogy elvigyél\negy itemet ingyen.");
+            }                
+
+            Console.WriteLine();
+
+            DrawMenu(7, shop);
+
+            if (Tutorial)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ez a bolt. Itt tudsz itemeket venni, amik bónuszt adnak a statiszt-\nikáidhoz. A szervezeted viszont egyszerre csak egy itemet bír, ami-\nnek a hatása a következő szobáig tart.");
+            }
+            Console.WriteLine("===================================================================");
+
+            ConsoleKeyInfo shopSelect = Console.ReadKey(true);
+            switch (shopSelect.Key)
+            {
                 case ConsoleKey.W:
-                    if (currentY > 0)
+                    shop[shopPoint, 0] = " ";
+                    if (shopPoint == 0)
                     {
-                        map[currentY, currentX] = '·';
-                        currentY--;
+                        shopPoint = 6;
+                    }
+                    else
+                    {
+                        shopPoint--;
                     }
                     break;
                 case ConsoleKey.S:
-                    if (currentY < height - 1)
+                    shop[shopPoint, 0] = " ";
+                    if (shopPoint == 6)
                     {
-                        map[currentY, currentX] = '·';
-                        currentY++;
+                        shopPoint = 0;
                     }
-                    break;
-                case ConsoleKey.A:
-                    if (currentX > 0)
+                    else
                     {
-                        map[currentY, currentX] = '·';
-                        currentX--;
-                    }
-                    break;
-                case ConsoleKey.D:
-                    if (currentX < width - 1)
-                    {
-                        map[currentY, currentX] = '·';
-                        currentX++;
+                        shopPoint++;
                     }
                     break;
                 case ConsoleKey.Enter:
-                    if (currentX == 0 && (currentY == 4 || currentY == 5))
-                    {
-                        ShopMenu();
-                    }
-                    else if (currentX == 9 && (currentY == 4 || currentY == 5))
-                    {
-                        CombatEnemy();
-                    }
-                    else if (currentY == 0 && (currentX == 4 || currentX == 5))
-                    {
-                        CombatBoss();
-                    }
+                    shopChoice = shopPoint + 1;
                     break;
                 case ConsoleKey.Escape:
                     MainMenu();
                     break;
             }
 
-            map[currentY, currentX] = 'O';
+            shop[shopPoint, 0] = ">";
 
-            DrawMap(map, width, height);
+            switch (shopChoice)
+            {
+                case 1:
+                    if (!hasItem)
+                    {
+                        Item1 = 2;
+                        hasItem = true;
+                    }
+                    break;
+                case 2:
+                    if (!hasItem)
+                    { 
+                        Item2 = 2;
+                        hasItem = true;
+                    }
+                    break;
+                case 3:
+                    if (!hasItem)
+                    {
+                        Item3 = 2;
+                        hasItem = true;
+                    }
+                    break;
+                case 4:
+                    if (!hasItem)
+                    {
+                        Item4 = 2;
+                        hasItem = true;
+                    }
+                    break;
+                case 5:
+                    if (!hasItem)
+                    {
+                        Item5 = 10;
+                        hasItem = true;
+                    }
+                    break;
+                case 6:
+                    if (!hasItem)
+                    {
+                        Item6 = 2;
+                        hasItem = true;
+                    }
+                    break;
+                case 7:
+                    Room = 0;
+                    hasItem = true;
+                    return;
+            }
         }
-    }
-
-    static void ShopMenu()
-    {
-        Console.Clear();
     }
 
     static void CombatEnemy()
     {
-        Console.Clear();
-        Console.WriteLine("enemy");
+        
     }
 
     static void CombatBoss()
