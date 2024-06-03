b10c5854a538698db62ba49c61211e66dbf007b4
Adamka0111 <62609739+Adamka0111@users.noreply.github.com>
2024-06-03 22:10:46 +0200

    Kész játék

---
+++ MuscleMayhemGame/MuscleMayhem/MuscleMayhem_Lib/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleMayhem_Lib
+{
+    public class Enemy
+    {
+        public Enemy()
+        {
+            Health = 100;
+            Energy = 20;
+            PunchLVL = 0;
+            BlockLVL = 0;
+            DodgeLVL = 0;
+            FlexLVL = 0;
+            FlexBonus = 0;
+            DodgeResult = false;
+        }
+
+        public int Health { get; set; }
+        public int Energy { get; set; }
+        public int PunchLVL { get; set; }
+        public int BlockLVL { get; set; }
+        public int DodgeLVL { get; set; }
+        public int FlexLVL { get; set; }
+        public int FlexBonus { get; set; }
+        public bool DodgeResult { get; set; }
+    }
+}
---
+++ MuscleMayhemGame/MuscleMayhem/MuscleMayhem_Lib/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleMayhem_Lib
+{
+    public class Shop
+    {
+        public Shop()
+        {
+            Item1 = 0;
+            Item2 = 0;
+            Item3 = 0;
+            Item4 = 0;
+            Item5 = 0;
+            Item6 = 0;
+        }
+
+        public int Item1 { get; set; }
+        public int Item2 { get; set; }
+        public int Item3 { get; set; }
+        public int Item4 { get; set; }
+        public int Item5 { get; set; }
+        public int Item6 { get; set; }
+    }
+}
